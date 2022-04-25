b86bacdacf04317c3a1e942fcd7dc2b660e437bc
wilmarPL <wilmarparrafeliciano@outlook.com>
2022-04-25 14:52:28 -0500

    primer repositorio

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,10 +3,34 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  let binario = num.split('').reverse()
+    let decimal = 0;
+    for(let i=0; i<binario.length; i++){
+        decimal += Math.pow(2,i) * binario[i];
+    }
+
+    return decimal; 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binario = [];
+  let cociente = num;
+
+  for(let i=0; i<=num; i++){
+      if(cociente/2 < 1){
+          binario.unshift(cociente);
+          break;
+      }
+      else{
+
+          binario.unshift(cociente%2); 
+          cociente = Math.floor(cociente/2)
+      }
+  }
+  return binario.join('');
+
+
 }
 
 // No se pueden usar: 
